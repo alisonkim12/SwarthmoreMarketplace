51d76a212382cd9b3d3dde5524a58484f218454d
Diego Hernandez <dmonsivais173@gmail.com>
2022-10-14 13:48:33 -0400

    added logout

--- backend/server.py
+++ backend/server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route('/logout')
+def bye():
+    return '<h1>Goodbye!</h1>'
+
 if __name__ == '__main__':
     app.run() # run locally
