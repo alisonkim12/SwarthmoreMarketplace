67f57c3b5afe94576fafe760ab4a71eb685f2ac8
Amy <ycho3@swarthmore.edu>
2022-11-09 16:07:15 -0500

    working on post items

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, session, render_template, request, redirect, send_from_directory
+from flask import Flask, flash, session, render_template, request, redirect, send_from_directory, url_for
+from werkzeug.utils import secure_filename
 import pyrebase
+import os
 import requests
 
 app = Flask(__name__)
@@ -155,6 +157,29 @@ def postItem():
             print(e)
             return 'Posting submission failed'
     return render_template('postItem.html')
+
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 
 
