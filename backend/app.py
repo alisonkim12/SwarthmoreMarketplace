398f73f5f293c15c8c7e61cbec4adc2b8be556c6
Alison Kim <skim7@swarthmore.edu>
2022-11-09 16:34:55 -0500

    worked on postItem function but does not work

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from werkzeug.utils import secure_filename
 import pyrebase
 import os
 import requests
+import datetime
 
 app = Flask(__name__)
 
@@ -20,6 +21,8 @@ config = {
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
+current_path = os.getcwd()
+
 
 app.secret_key = 'secret'
 
@@ -114,34 +117,39 @@ def get_user_info():
 
 @app.route('/postItem', methods = ['GET', 'POST'])
 def postItem():
-    item_name = request.form.get('item_name')
-    item_price = request.form.get('item_price')
-    item_condition = request.form.get('item_condition')
-    item_description = request.form.get('item_description')
-
-    #storing images to the backend
-    """
-    upload_path = current_path + '/images'
-    def upload_file():
-        if request.method == 'POST':
-            photo_file = request.files['item_photo"']
-            if photo_file.filename != '':
-                file_extension = photo_file.filename.split(".")[-1]
-                # must create or get a product_id
-                output_filepath = upload_path + '/templates/' + product_id + '.' + file_extension
-                photo_file.save(output_filepath) #saves file to uploaded file
-                return render_template("next.html")
-            else:
-                return 'failure'
-        else:
-            return render_template("current.html")
-
-    """
-
-    #idk how photos uploaded from the frontend translates into the backend and how to store the data
     if request.method == 'POST': #if form submitted
         try: #register
             #user = auth.create_user_with_email_and_password(email, password)
+         
+            if ('user' in session):
+                user_email = session['user']
+                try:
+                    user_object = db.child("users").order_by_child("email").equal_to(user_email).get()
+                    user_info = None
+                    for user in user_object.each(): #figure out if there's an easier way to do this
+                        user_info = user.val()
+                except Exception as e:
+                    print(e)
+                    return 'Failed to get user info'
+            
+            item_name = request.form.get('item_name')
+            item_price = request.form.get('item_price')
+            item_condition = request.form.get('item_condition')
+            item_description = request.form.get('item_description')
+            seller_fname = user_info.get('firstname')
+            seller_lname = user_info.get('lastname')
+            seller_email = user_info.get('email')
+            posting_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        
+            product_id = seller_lname + seller_email + posting_time
+            
+            #storing images to the backend
+            f = request.files['item_photo']
+            if f.filename != '':
+                file_extension = f.filename.split(".")[-1]
+                output_filepath = current_path + '/static/uploads/' + product_id + '.' + file_extension
+                f.save(output_filepath) #saves file to uploaded file
+
             posting_data = {
                 "firstname": user_info.get('firstname'),
                 "lastname": user_info.get('lastname'),
@@ -149,37 +157,54 @@ def postItem():
                 "item_name": item_name,
                 "item_price": item_price, 
                 "item_condition": item_condition,
-                "item_description": item_description
-                #item_photo: item_photo? if it is inside the templates file we can access it that way instead of storing it in the firebase console 
+                "item_description": item_description,
+                "file_path" : output_filepath
             }
             db.child("postings").push(posting_data, user['idToken'])
+
         except Exception as e:
             print(e)
             return 'Posting submission failed'
     return render_template('postItem.html')
 
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+   
+   
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+    """def upload_file():
+        if request.method == 'POST':
+            
+                return render_template("next.html")
+            else:
+                return 'failure'
+        else:
+            return render_template("current.html")"""
 
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    """UPLOAD_FOLDER = '/path/to/the/uploads'
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+    app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+
+    def upload_file():
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit a empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))"""
+
+    #idk how photos uploaded from the frontend translates into the backend and how to store the data
+    
+
 
 
 
