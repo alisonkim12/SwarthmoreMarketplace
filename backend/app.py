319a98699f1f7806fe4e72e960a3b20ada090028
Amy <ycho3@swarthmore.edu>
2022-11-11 12:02:02 -0500

    working on posting items

--- backend/app.py
+++ backend/app.py
@@ -134,7 +134,8 @@ def postItem():
             product_id = seller_lname + seller_email + posting_time
             
             #storing images to the backend
-            f = request.files['item_photo']
+            upload_file()
+            '''f = request.files['item_photo']
             if f.filename != '':
                 file_extension = f.filename.split(".")[-1]
                 output_filepath = current_path + '/static/uploads/' + product_id + '.' + file_extension
@@ -152,6 +153,7 @@ def postItem():
             }
             #THE LINE BELOW IS BUGGED BUT I THINK THE ERROR IS SOMETHING DIFFERENT FOR NOW
             db.child("postings").push(posting_data, user['idToken'])
+            '''
 
         except Exception as e:
             print(e)
@@ -159,44 +161,26 @@ def postItem():
     return render_template('postItem.html')
 
 
-   
-   
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-    """def upload_file():
-        if request.method == 'POST':
-            
-                return render_template("next.html")
-            else:
-                return 'failure'
-        else:
-            return render_template("current.html")"""
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 
-    """UPLOAD_FOLDER = '/path/to/the/uploads'
-    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
-    app = Flask(__name__)
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
-
-    def upload_file():
-        if request.method == 'POST':
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            # if user does not select file, browser also
-            # submit a empty part without filename
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))"""
-
-    #idk how photos uploaded from the frontend translates into the backend and how to store the data
-    
-
-
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 
 @app.route('/public/stylesheets/styles.css')
